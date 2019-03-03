1aa0660cec9de52303aef4cf9b888c52fffd6604
europestar <makarov.tim.dev@gmail.com>
2019-03-04 00:05:33 +0300

    Added SQL update for task1

---
+++ scripts/task1/update.sql
@@ -0,0 +1,11 @@
+update fillfacor_50
+set message = message || message
+
+update fillfacor_75
+set message = message || message
+
+update fillfacor_90
+set message = message || message
+
+update fillfacor_100
+set message = message || message
