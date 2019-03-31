40b2194c093c4ce95aac928a05f96d3ce72e5e01
europestar <makarov.tim.dev@gmail.com>
2019-03-31 22:20:26 +0300

    added vacuum

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 # from task1 import fiffactor_insert as task
-from task_3_2 import app as task
+# from task_3_2 import app as task
+from lab_vacuum import update as task
 
 if __name__ == '__main__':
     task.resolve()
